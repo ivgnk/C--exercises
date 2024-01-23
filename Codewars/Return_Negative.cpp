36ce01d8d476b935114021867608ddcb22a1211f
Ivan Genik <IVGenik@rambler.ru>
2024-01-23 16:12:02 +0500

    Add files via upload

---
+++ Codewars/Return_Negative.cpp
@@ -0,0 +1,13 @@
+int makeNegative(int num)
+// Examples
+// makeNegative(1);  // return -1
+// makeNegative(-5); // return -5
+// makeNegative(0);  // return 0
+{
+  if (num==0)
+    { return 0; } 
+  else if (num<0)
+    { return num; }
+  else  
+    {return -num; }
+  }
