d3811d3a65ba5d0c72e45ae27e274e3fd143656d
Ivan Genik <IVGenik@rambler.ru>
2024-01-31 18:04:13 +0500

    Add files via upload

--- Test/inp_out.cpp
+++ Test/inp_out.cpp
@@ -1,20 +1,23 @@
 #include <iostream> 
 #include <string> 
-#include "windows.h"
+#include <windows.h>
 using namespace std; 
   
 // Driver Code 
 int main() 
 { 
     int i; 
+    int tm = 6000;
     string s;
     // Take input using cin 
-    cout << "input any char "<< endl;
+    cout << "With <windows.h>" << endl; 
+    cout << "input any char "<< endl; // <iostream> 
     cin >> s; 
-  
+ 
     // Print output 
     cout << "You input char is  "+s << endl; 
-    cout << "done" << endl; 
-    Sleep(6000); // "windows.h"
+
+    cout << "done in "+std::to_string(tm)+" sec" << endl; 
+    Sleep(tm); // "windows.h"
     return 0; 
 }
