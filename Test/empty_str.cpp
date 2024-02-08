58727019cf54b94c1d4dc5dbf25a32cec60ec3df
Ivan Genik <IVGenik@rambler.ru>
2024-02-08 19:52:29 +0500

    Add files via upload

---
+++ Test/empty_str.cpp
@@ -0,0 +1,15 @@
+/*
+
+*/
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    system("cls");
+    string s="";
+    cout << "Result" <<endl;
+    cout << "size = "+to_string(s.size()) <<endl;
+    cout << "empty = "+to_string(s.empty()) <<endl;
+}
